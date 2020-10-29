8cdf7c19170cc10c88ab6df8b72a92dd06defd5b
Sebastian Wnęk <kontakt@wnekofoty.pl>
2020-10-29 08:16:14 +0100

    Dodaj pliki projektów.

---
+++ ComPortTest/MeadowApp.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using Meadow;
+using Meadow.Devices;
+using Meadow.Foundation;
+using Meadow.Foundation.Displays.Lcd;
+using Meadow.Foundation.Leds;
+using Meadow.Hardware;
+
+namespace ComPortTest
+{
+    public class MeadowApp : App<F7Micro, MeadowApp>
+    {
+        RgbPwmLed onboardLed;
+        ISerialMessagePort port;
+        CharacterDisplay display;
+        II2cBus bus;
+        
+
+        public MeadowApp()
+        {
+            Initialize();
+            onboardLed.SetColor(Color.Green);
+        }
+
+        void Initialize()
+        {
+            Console.WriteLine("Initialize hardware...");
+
+            onboardLed = new RgbPwmLed(device: Device,
+                redPwmPin: Device.Pins.OnboardLedRed,
+                greenPwmPin: Device.Pins.OnboardLedGreen,
+                bluePwmPin: Device.Pins.OnboardLedBlue,
+                3.3f, 3.3f, 3.3f,
+                Meadow.Peripherals.Leds.IRgbLed.CommonType.CommonAnode);
+
+            bus = Device.CreateI2cBus();
+
+            display = new CharacterDisplay(bus, 39, 2, 16);
+
+            port = Device.CreateSerialMessagePort(Device.SerialPortNames.Com4, suffixDelimiter: new byte[] { 10 }, preserveDelimiter: true, 1000000, 8, Parity.None, StopBits.One);
+            port.Open();
+            port.MessageReceived += Port_MessageReceived;
+        }
+
+        private void Port_MessageReceived(object sender, SerialMessageData e)
+        {
+            string msg = Encoding.UTF8.GetString(e.Message);
+            foreach (byte b in msg) Console.WriteLine(b);
+            Console.WriteLine(msg);
+            display.ClearLines();
+            display.Write(msg);
+            onboardLed.SetColor(Color.Red);             //So i knew it was receiving data before I connected display
+            Thread.Sleep(500);
+            onboardLed.SetColor(Color.Blue);
+            port.Write(Encoding.UTF8.GetBytes($"Received and acknowledging! Msg: {msg}"));  //There is small issue that messages contain LF at the end and it's sent back too
+                                                                                            //but nah, in target use it won't matter
+
+        }
+    }
+}
---
+++ ComPortTestPC/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComPortTestPC
+{
+    class Program
+    {
+        static SerialPort port;
+        static void Main(string[] args)
+        {
+            port = new SerialPort("COM4", 1000000, Parity.None, 8, StopBits.One);
+            port.Open();
+            while (true)
+            {
+                string message = Console.ReadLine();
+                port.WriteLine(message);
+                port.DataReceived += Port_DataReceived;
+            }
+        }
+
+        private static void Port_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            Console.WriteLine(port.ReadLine());
+        }
+    }
+}
